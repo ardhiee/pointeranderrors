028c5fef07eed9866da4bcc019b60f40a393d480
Ardi Ismail <>
2019-10-01 18:44:31 +0700

    add withdrawn func

--- wallet.go
+++ wallet.go
@@ -1,17 +1,35 @@
 package pointeranderrors
 
+import "fmt"
+
+// Bitcoin type
+type Bitcoin int
+
 // Wallet type
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-// Balance will return the balance
-func (w *Wallet) Balance() int {
-	return w.balance
+// Stringer only return string
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 // Deposit will return the amount of deposit
-func (w *Wallet) Deposit(amount int) int {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+// Withdraw will deduct amount
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+// Balance will return the balance
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
